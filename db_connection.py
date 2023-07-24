da3f2376e30c6cebc51860377ea75dbf5bf20464
sgomezp <sgomezp@gmail.com>
2023-07-24 18:15:08 +0200

    Protegemos los datos de conexión a al BD

--- db_connection.py
+++ db_connection.py
@@ -1,4 +1,6 @@
+import os
 import psycopg
+from dotenv import load_dotenv
 
 
 class DataBaseConnection:
@@ -7,13 +9,20 @@ class DataBaseConnection:
         """
         Constructor. Conecta con la DB
         """
+        load_dotenv()  # Carga las variables de entorno desde el archivo .env
+        db_name = os.getenv("DB_NAME")
+        db_user = os.getenv("DB_USER")
+        db_password = os.getenv("DB_PASSWORD")
+        db_host = os.getenv("DB_HOST")
+        db_port = os.getenv("DB_PORT")
+
         try:
             self.conn = psycopg.connect(
-                dbname="Fenix",
-                user="postgres",
-                password="9527",
-                host="localhost",
-                port=5432
+                dbname=db_name,
+                user=db_user,
+                password=db_password,
+                host=db_host,
+                port=db_port
             )
         except psycopg.OperationalError as err:
             print(err)
