ffae89637baeee8a4f234827fca9e50a02005231
Victoria-moraleda <victoriamoraledablancoi@gmail.com>
2023-07-26 11:41:16 +0200

    control de excepciones OK

--- model/alumnos_clases.py
+++ model/alumnos_clases.py
@@ -10,10 +10,15 @@ class AlumnosClases():
         CRUD READ. Lee todos los registros de la tabla Alumnos_clases
         :return:
         """
-        with self.conn.cursor() as cur:
-            cur.execute("""SELECT * FROM "Alumnos_clases" """)
-            data = cur.fetchall()
-            return data
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute("""SELECT * FROM "Alumnos_clases" """)
+                data = cur.fetchall()
+                return data
+        except psycopg.Error as e:
+            # Manejo de la excepción específica para errores de lectura en la base de datos
+            print("Error al leer los registros de la tabla Alumnos_clases:", e)
+            return None
 
     def insert_alumno_clase(self, data):
         """
@@ -21,12 +26,16 @@ class AlumnosClases():
         :param data:
         :return:
         """
-        with self.conn.cursor() as cur:
-            cur.execute("""
-                INSERT INTO "Alumnos_clases"(alumno_id, clase_id) 
-                VALUES (%(alumno_id)s, %(clase_id)s)
-            """, data)
-            self.conn.commit()
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute("""
+                    INSERT INTO "Alumnos_clases"(alumno_id, clase_id) 
+                    VALUES (%(alumno_id)s, %(clase_id)s)
+                """, data)
+                self.conn.commit()
+        except psycopg.Error as e:
+            # Manejo de la excepción específica para errores al insertar registros en la base de datos
+            print("Error al insertar el registro en la tabla Alumnos_clases:", e)
 
     def delete(self, alumno_id: int, clase_id: int):
         """
@@ -35,11 +44,16 @@ class AlumnosClases():
         :param clase_id: int
         :return:
         """
-        with self.conn.cursor() as cur:
-            cur.execute("""
-                DELETE FROM "Alumnos_clases" WHERE alumno_id = %(alumno_id)s AND clase_id = %(clase_id)s
-            """, {"alumno_id": alumno_id, "clase_id": clase_id})
-            self.conn.commit()
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute("""
+                    DELETE FROM "Alumnos_clases" WHERE alumno_id = %(alumno_id)s AND clase_id = %(clase_id)s
+                """, {"alumno_id": alumno_id, "clase_id": clase_id})
+                self.conn.commit()
+        except psycopg.Error as e:
+            # Manejo de la excepción específica para errores al eliminar registros en la base de datos
+            print("Error al eliminar el registro de la tabla Alumnos_clases:", e)
+
 
 
     def __del__(self):
